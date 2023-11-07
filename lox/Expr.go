7861713fcc1e915eeabd9381a2b45eced0939877
matt <mnicholson110@gmail.com>
2023-11-07 11:18:02 -0600

    WIP: Chapter 5

---
+++ lox/Expr.go
@@ -0,0 +1,22 @@
+package lox
+
+type Expr interface{}
+
+type Binary struct {
+	left     Expr
+	operator Token
+	right    Expr
+}
+
+type Grouping struct {
+	expression Expr
+}
+
+type Literal struct {
+	value interface{}
+}
+
+type Unary struct {
+	operator Token
+	right    Expr
+}
